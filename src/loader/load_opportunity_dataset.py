7ac9c4c0b6d62c4e9b639bbe0d303979848e4a09
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2022-04-13 16:46:19 +0200

    do some linting

--- src/loader/load_opportunity_dataset.py
+++ src/loader/load_opportunity_dataset.py
@@ -71,7 +71,12 @@ def load_opportunity_dataset(opportunity_dataset_path: str) -> "list[Recording]"
         "IMU-R-SHOE-AngVelNavFrameY",
         "IMU-R-SHOE-AngVelNavFrameZ",
     ]
-    print(f"Selected features (n_features: {len(selected_feature_names)}):\n", "\n".join(["\t" + str(feature_name) for feature_name in selected_feature_names]))
+    print(
+        f"Selected features (n_features: {len(selected_feature_names)}):\n",
+        "\n".join(
+            ["\t" + str(feature_name) for feature_name in selected_feature_names]
+        ),
+    )
 
     # Get column names
     col_names = []
@@ -86,18 +91,19 @@ def load_opportunity_dataset(opportunity_dataset_path: str) -> "list[Recording]"
         file_path = os.path.join(opportunity_dataset_path, file_name)
         print(f"Reading {file_path} ...")
         file_df = pd.read_csv(file_path, delimiter=" ", header=None)
-        file_df.columns = col_names # give them the real column names
+        file_df.columns = col_names  # give them the real column names
 
-        recordings.append(Recording(
-            sensor_frame = file_df.loc[:, selected_feature_names], 
-            time_frame = file_df.loc[:, 'MILLISEC'],
-            activities = file_df.loc[:, 'HL_Activity'].map(
-                lambda label: settings.activity_initial_num_to_activity_idx[label]
-            ),  # Use `[0]` to get only one activity | maps 0, 101, 102, 103, 104, 105 to 0, 1, 2, 3, 4, 5
-            subject = f"{sub}",
-        ))
+        recordings.append(
+            Recording(
+                sensor_frame=file_df.loc[:, selected_feature_names],
+                time_frame=file_df.loc[:, "MILLISEC"],
+                activities=file_df.loc[:, "HL_Activity"].map(
+                    lambda label: settings.activity_initial_num_to_activity_idx[label]
+                ),  # Use `[0]` to get only one activity | maps 0, 101, 102, 103, 104, 105 to 0, 1, 2, 3, 4, 5
+                subject=f"{sub}",
+            )
+        )
 
     print(f"\n => Total {len(recordings)} recordings read")
 
     return recordings
-
--- src/tests/test_original_jens_windowize.py
+++ src/tests/test_original_jens_windowize.py
@@ -2,6 +2,7 @@ import pandas as pd
 from models.JensModel import JensModel
 from utils.Recording import Recording
 
+
 def jens_windowize(dataCollection, window_size):
     """
     in the working version expects dataframe with these columns:
@@ -26,7 +27,7 @@ def jens_windowize(dataCollection, window_size):
         'IMU-R-SHOE-AngVelNavFrameX', 'IMU-R-SHOE-AngVelNavFrameY',
         'IMU-R-SHOE-AngVelNavFrameZ', 'Locomotion', 'HL_Activity',
         'file_index']
-    
+
     test subset:
 
         ['IMU-BACK-accX', 'IMU-BACK-accY', 'IMU-BACK-accZ', 'Locomotion', 'HL_Activity', 'file_index']
@@ -49,13 +50,15 @@ def jens_windowize(dataCollection, window_size):
         # what if it changes back and forth?
         if (
             data[start][HL_Activity_i] == data[end][HL_Activity_i]
-            and data[start][-1] == data[end][-1] # the last index is the file index
+            and data[start][-1] == data[end][-1]  # the last index is the file index
         ):
 
             # print(data[start:(end+1),0:(HL_Activity_i)])
             # first part time axis, second part sensor axis -> get window
             X.append(
-                data[start : (end + 1), 0 : (HL_Activity_i - 1)] # data[timeaxis/row, featureaxis/column] data[1, 2] gives specific value, a:b gives you an interval
+                data[
+                    start : (end + 1), 0 : (HL_Activity_i - 1)
+                ]  # data[timeaxis/row, featureaxis/column] data[1, 2] gives specific value, a:b gives you an interval
             )  # slice before locomotion
             y.append(data[start][HL_Activity_i])  # the first data point is enough
             start += window_size // 2  # 50% overlap!!!!!!!!!
@@ -73,32 +76,230 @@ def jens_windowize(dataCollection, window_size):
 
     return X, y
 
+
 # same example_recordings (first recording duplicated) from test_our_jens_windowize.py
 # same window expectation!!!!
 
 example_recordings = [
     Recording(
         # 30 timesteps, 3 features
-        sensor_frame = pd.DataFrame([[1,11,111],[2,22,222],[3,33,333],[4,44,444],[5,55,555], [6,66,666], [7,77,777], [8,88,888], [9,99,999], [10,100,1000], [11, 111, 1111], [12, 222, 2222], [13, 333, 3333], [14, 444, 4444], [15, 555, 5555], [16, 666, 6666], [17, 777, 7777], [18, 888, 8888], [19, 999, 9999], [20, 1000, 10000], [21, 111, 1111], [22, 222, 2222], [23, 333, 3333], [24, 444, 4444], [25, 555, 5555], [26, 666, 6666], [27, 777, 7777], [28, 888, 8888], [29, 999, 9999], [30, 1000, 10000]]),
-        time_frame = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]),
-        activities = pd.Series([0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]),
-        subject = "Bruder Jakob"
+        sensor_frame=pd.DataFrame(
+            [
+                [1, 11, 111],
+                [2, 22, 222],
+                [3, 33, 333],
+                [4, 44, 444],
+                [5, 55, 555],
+                [6, 66, 666],
+                [7, 77, 777],
+                [8, 88, 888],
+                [9, 99, 999],
+                [10, 100, 1000],
+                [11, 111, 1111],
+                [12, 222, 2222],
+                [13, 333, 3333],
+                [14, 444, 4444],
+                [15, 555, 5555],
+                [16, 666, 6666],
+                [17, 777, 7777],
+                [18, 888, 8888],
+                [19, 999, 9999],
+                [20, 1000, 10000],
+                [21, 111, 1111],
+                [22, 222, 2222],
+                [23, 333, 3333],
+                [24, 444, 4444],
+                [25, 555, 5555],
+                [26, 666, 6666],
+                [27, 777, 7777],
+                [28, 888, 8888],
+                [29, 999, 9999],
+                [30, 1000, 10000],
+            ]
+        ),
+        time_frame=pd.Series(
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+                29,
+                30,
+            ]
+        ),
+        activities=pd.Series(
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                0,
+            ]
+        ),
+        subject="Bruder Jakob",
     ),
     Recording(
         # 30 timesteps, 3 features
-        sensor_frame = pd.DataFrame([[1,11,111],[2,22,222],[3,33,333],[4,44,444],[5,55,555], [6,66,666], [7,77,777], [8,88,888], [9,99,999], [10,100,1000], [11, 111, 1111], [12, 222, 2222], [13, 333, 3333], [14, 444, 4444], [15, 555, 5555], [16, 666, 6666], [17, 777, 7777], [18, 888, 8888], [19, 999, 9999], [20, 1000, 10000], [21, 111, 1111], [22, 222, 2222], [23, 333, 3333], [24, 444, 4444], [25, 555, 5555], [26, 666, 6666], [27, 777, 7777], [28, 888, 8888], [29, 999, 9999], [30, 1000, 10000]]),
-        time_frame = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]),
-        activities = pd.Series([0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]),
-        subject = "Bruder Jakob 2"
+        sensor_frame=pd.DataFrame(
+            [
+                [1, 11, 111],
+                [2, 22, 222],
+                [3, 33, 333],
+                [4, 44, 444],
+                [5, 55, 555],
+                [6, 66, 666],
+                [7, 77, 777],
+                [8, 88, 888],
+                [9, 99, 999],
+                [10, 100, 1000],
+                [11, 111, 1111],
+                [12, 222, 2222],
+                [13, 333, 3333],
+                [14, 444, 4444],
+                [15, 555, 5555],
+                [16, 666, 6666],
+                [17, 777, 7777],
+                [18, 888, 8888],
+                [19, 999, 9999],
+                [20, 1000, 10000],
+                [21, 111, 1111],
+                [22, 222, 2222],
+                [23, 333, 3333],
+                [24, 444, 4444],
+                [25, 555, 5555],
+                [26, 666, 6666],
+                [27, 777, 7777],
+                [28, 888, 8888],
+                [29, 999, 9999],
+                [30, 1000, 10000],
+            ]
+        ),
+        time_frame=pd.Series(
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+                29,
+                30,
+            ]
+        ),
+        activities=pd.Series(
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                0,
+            ]
+        ),
+        subject="Bruder Jakob 2",
     ),
     Recording(
         # only one timestep
-        sensor_frame = pd.DataFrame([[1, 11, 111]]),
-        time_frame = pd.Series([1]),
-        activities = pd.Series([0]), 
-        subject = "Schwester Hortensie"
+        sensor_frame=pd.DataFrame([[1, 11, 111]]),
+        time_frame=pd.Series([1]),
+        activities=pd.Series([0]),
+        subject="Schwester Hortensie",
     ),
-    
 ]
 
 # adapt to to format expected by jens_windowize
@@ -106,18 +307,41 @@ dataCollection = pd.DataFrame()
 for i, recording in enumerate(example_recordings):
     n_timesteps = len(recording.sensor_frame)
 
-    locomotion_series = pd.Series([42] * n_timesteps) # will be filtered out
+    locomotion_series = pd.Series([42] * n_timesteps)  # will be filtered out
     file_index_series = pd.Series([i] * n_timesteps)
 
-    recording_df = pd.concat([recording.sensor_frame, locomotion_series, recording.activities, file_index_series], axis=1)
-    recording_df.columns = ['IMU-BACK-accX', 'IMU-BACK-accY', 'IMU-BACK-accZ', 'Locomotion', 'HL_Activity', 'file_index']
+    recording_df = pd.concat(
+        [
+            recording.sensor_frame,
+            locomotion_series,
+            recording.activities,
+            file_index_series,
+        ],
+        axis=1,
+    )
+    recording_df.columns = [
+        "IMU-BACK-accX",
+        "IMU-BACK-accY",
+        "IMU-BACK-accZ",
+        "Locomotion",
+        "HL_Activity",
+        "file_index",
+    ]
 
     dataCollection = pd.concat([dataCollection, recording_df])
 
 
 # start test
 window_size = 5
-window_start_idx = [1, 3, 11, 13, 21, 23, 25] # from other test to confirm the right cutting
+window_start_idx = [
+    1,
+    3,
+    11,
+    13,
+    21,
+    23,
+    25,
+]  # from other test to confirm the right cutting
 
 X, y = jens_windowize(dataCollection, window_size)
 print(X)
