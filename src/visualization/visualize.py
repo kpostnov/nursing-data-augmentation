1342502e4c5e3263b3fb9e52387b0b43117425cf
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2022-05-02 22:35:03 +0200

    Add t-SNE visualization

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 from typing import Dict
 import math
 import numpy as np
+from sklearn.manifold import TSNE
 
 
 def plot_pca_distribution(original_data: np.ndarray, generated_data: np.ndarray, activity: str, ax = None) -> None:
@@ -148,8 +149,58 @@ def plot_pca_column_wise(windows: 'list[Window]', columns_to_plot: Dict[int, str
     print('Saved pca_columns.png')
 
 
-def plot_tsne(windows: 'list[Window]', columns_to_plot: Dict[int, str]) -> None:
+def plot_tsne_distribution(original_data: np.ndarray, generated_data: np.ndarray, activity: str, ax = None) -> None:
     '''
-    columns_to_plot is a dictionary mapping the index of the sensor channel (column) 
-    to the name of the sensor channel.
+    Plot distributions of the original and generated data using t-SNE.
     '''
+    
+    assert(original_data.ndim == 4), 'original_data must be a 4D array'
+    assert(generated_data.ndim == 4), 'generated_data must be a 4D array'
+
+    number_samples = min(original_data.shape[0], 2000)
+    idx = np.random.permutation(number_samples)
+
+    original_data = original_data[idx]
+    generated_data = generated_data[idx]
+
+    original_data = np.squeeze(original_data, axis=-1)
+    generated_data = np.squeeze(generated_data, axis=-1)
+
+    seq_len = original_data.shape[1]
+
+    for i in range(number_samples):
+        if (i == 0):
+            original_array = np.reshape(np.mean(original_data[0,:,:], 1), [1, seq_len])
+            generated_array = np.reshape(np.mean(generated_data[0,:,:], 1), [1, seq_len])
+        else:
+            original_array = np.concatenate((original_array, 
+                                        np.reshape(np.mean(original_data[i,:,:], 1), [1, seq_len])))
+            generated_array = np.concatenate((generated_array, 
+                                        np.reshape(np.mean(generated_data[i,:,:], 1), [1, seq_len])))
+
+
+    df_original = pd.DataFrame(original_array)
+    df_generated = pd.DataFrame(generated_array)
+    df_original['color'] = 'red'
+    df_generated['color'] = 'blue'
+
+    prep_data_final = pd.concat([df_original, df_generated], ignore_index=True)
+    
+    # TSNE anlaysis
+    tsne = TSNE(n_components = 2, verbose = 1, perplexity = 40, n_iter = 300)
+    tsne_results = tsne.fit_transform(prep_data_final.iloc[:, :-1])
+      
+    # Plotting
+    f, ax = plt.subplots(1)
+      
+    plt.scatter(tsne_results[:number_samples,0], tsne_results[:number_samples,1], 
+                c = prep_data_final.loc[:number_samples-1, 'color'], alpha = 0.2, label = "Original")
+    plt.scatter(tsne_results[number_samples:,0], tsne_results[number_samples:,1], 
+                c = prep_data_final.loc[number_samples:, 'color'], alpha = 0.2, label = "Synthetic")
+  
+    ax.legend()
+      
+    plt.title('t-SNE visualization of activity: ' + activity)
+    plt.savefig(f'visualization/plots/tsne_distribution_{activity}.png')
+    
+    print(f'Plotting t-SNE visualization of activity {activity} finished') 
